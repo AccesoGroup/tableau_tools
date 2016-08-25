01712562d6256e4d344dafa3ddd30d44c5c8a5a7
Miguel Gonzalez <miguelgonzalez@acceso.com>
2016-08-25 13:39:58 +0200

    Fix setup.py to install directly from github

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='tableau_tools',
     version='3.0.1',
     install_requires=['lxml'],
-    packages=['tableau_tools', 'tableau_tools.tableau_rest_api', 'tableau_tools.tableau_documents'],
+    packages=find_packages(),
     url='https://github.com/bryantbhowell/tableau_tools',
     license='',
     author='Bryant Howell',
